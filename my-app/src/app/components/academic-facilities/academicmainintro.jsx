c96879f791f20e7876802c424f37c5caec061dfa
sujal123-abc <sujalkothawade2803@gmail.com>
2025-12-21 13:23:15 +0530

    changes made by sujal

--- my-app/src/app/components/academic-facilities/academicmainintro.jsx
+++ my-app/src/app/components/academic-facilities/academicmainintro.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import ResearchIntro from '../research-resources/research_intro'
+import MainIntro from '../about/main_intro'
 
 function Academicmainintro() {
     return (
         <>
-            <ResearchIntro
+            <MainIntro
                 title="Academic Facilities"
                 subtitle="Experience A Supportive Learning Environment"
                 description={["We nurture our students by providing them with all the essential academic facilities and support they need to excel in their chosen fields. They don’t just learn theoretical knowledge, but also learn from practical exposure and an experiential learning environment. With our hands-on learning experience, they become confident and ready to take up challenges and stand out as a better individual."
                 ]}
                 imageUrl="https://kalinga-university.s3.ap-south-1.amazonaws.com/common/placeholder-img.png" //change image
                 imageAlt="Academic Facilities"
+                readMoreLabel="Read More"
+                readLessLabel="Read Less"
+                showKnowMore={true}
+                initialVisibleParagraphs={1}
             />
         </>
     )
