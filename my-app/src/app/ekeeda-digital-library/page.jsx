7c109b4e285c70a0e8e17f11c927d9041d20b90e
amul-rs <amulrs112@gmail.com>
2026-01-13 19:56:54 +0530

    deploy  ifx

---
+++ my-app/src/app/ekeeda-digital-library/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react';
+
+export default function EkeedaPage() {
+    return (
+        <div>
+            <h1>Ekeeda Digital Library</h1>
+        </div>
+    );
+}
